363297536e30e90d5e628ccd8a7374453ec6e463
alinko <alinkokomansuby@gmail.com>
2017-03-18 14:46:09 +0700

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -3,11 +3,13 @@
 source /etc/db_backres/db_alinko.conf
 
 restore_db(){
+		echo "========================================================" >> /var/log/db_backup.log
 	cat $backupdbs > /dev/null 2>&1
 	if [[ $? -eq 0 ]]; then
 	 mysql -h $hostname_a -u $username_a -p$password_a -e 'CREATE DATABASE '$db_name'' 
 	 mysql -h $hostname_a -u $username_a -p$password_a $db_name -e 'source '$backupdbs''
 	 if [[ $? -eq 0 ]]; then
+
 	 	echo "SUCCESS RESTORE DATABASE "$db_name" ;) ["$formatgl"] " >>  /var/log/db_backup.log
 	 else
 	 	echo "FAILED RESTORE DATABASE "$db_name" :( ["$formatgl"] Problem Found In DATABASE Connection." >> /var/log/db_backup.log
@@ -16,6 +18,7 @@ restore_db(){
 	 echo "FAILED RESTORE DATABASE "$db_name" File Backup Not Found ["$formatgl"]." >> /var/log/db_backup.log
 
 	fi
+		echo "========================================================" >> /var/log/db_backup.log
 }
 
 mysql -h $hostname_a -u $username_a -p$password_a -e 'USE '$db_name'' > /dev/null 2>&1
