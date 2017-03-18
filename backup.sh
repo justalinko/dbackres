7b14ec3e70e64d697d90fb3723b105363a1bd8a0
alinko <alinkokomansuby@gmail.com>
2017-03-18 14:45:13 +0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -3,13 +3,15 @@
 source /etc/db_backres/db_alinko.conf
 
 backup_db(){
-mysqldump -h $hostname_a -u $username_a -p$password_a $db_name > $backupdbs
+		echo "========================================================" >> /var/log/db_backup.log
 mysqldump -h $hostname_a -u $username_a -p$password_a $db_name > $backupdb
 if [[ $? -eq 0 ]]; then
  	echo "SUCCESS BACKUP DATABASE ["$formatgl"] --> "$backupdb >> /var/log/db_backup.log
+ 	mysqldump -h $hostname_a -u $username_a -p$password_a $db_name > $backupdbs
  else
  	echo "FAILED BACKUP DATABASE ["$formatgl"] --> "$backupdb >> /var/log/db_backup.log
  fi 
+ 		echo "========================================================" >> /var/log/db_backup.log
 
 }
 backup_db
