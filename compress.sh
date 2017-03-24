d48daadf4e3642cdb07582475a9295b63700afc3
alinko <alinkokomansuby@gmail.com>
2017-03-24 23:32:11 +0700

    Update compress.sh

--- compress.sh
+++ compress.sh
@@ -3,9 +3,9 @@
 source /etc/db_backres/db_alinko.conf
 
 cek_jml=`ls /var/db_backres/backup | wc -l`
-if [[ $cek_jml > 2 ]]; then
+if [[ $cek_jml -gt 10 ]]; then
 	zip -r /var/db_backres/backup/bak_$formatgl.zip /var/db_backres/backup > /dev/null 2>&1
-	rm /var/db_backres/backup/*.sql
+	rm /var/db_backres/backup/db-backup-alinko*.sql
 	echo "========================================================" >> /var/log/db_backup.log
 	echo "FILES BACKUP COUNT AT [ "$formatgl" ] :: "$cek_jml" :: " >> /var/log/db_backup.log  
 	echo "COMPRESSED /var/db_backres/backup/bak_"$formatgl".zip" >> /var/log/db_backup.log
